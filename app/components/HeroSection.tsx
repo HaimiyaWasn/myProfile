8b63091b46ebb531b07786fcd62e70206a892c5b
WAHYU AJI SETYO NUGROHO <wahyuajisn@gmail.com>
2026-06-20 15:25:29 +0700

    Update code

--- app/components/HeroSection.tsx
+++ app/components/HeroSection.tsx
@@ -62,7 +62,7 @@ export default function HeroSection() {
                 animate={{
                   pathLength: [0, 1, 1, 0, 0],
                   pathOffset: [0, 0, 0, 1, 1],
-                  opacity: [1, 1, 1, 1, 0, 0],
+                  opacity: [0, 1, 1, 1, 0, 0],
                 }}
                 transition={{
                   duration: 17,
@@ -110,6 +110,80 @@ export default function HeroSection() {
                   mode="normal"
                   in="SourceGraphic"
                   in2="effect1_dropShadow_2443_91"
+                  result="shape"
+                />
+              </filter>
+            </defs>
+          </svg>
+        )}
+
+        {showLine && (
+          <svg
+            className="hidden md:block absolute right-0 top-0 w-full max-w-114 h-auto"
+            width="456"
+            height="361"
+            viewBox="0 0 456 361"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <g filter="url(#filter0_d_2453_145)">
+              <motion.path
+                initial={{
+                  pathLength: 0,
+                  pathOffset: 0,
+                  opacity: 0,
+                }}
+                animate={{
+                  pathLength: [0, 1, 1, 0, 0],
+                  pathOffset: [0, 0, 0, 1, 1],
+                  opacity: [0, 1, 1, 1, 0, 0],
+                }}
+                transition={{
+                  delay: 1,
+                  duration: 10,
+                  times: [0, 0.25, 0.5, 0.75, 0.751, 1],
+                  ease: "linear",
+                  repeat: Infinity,
+                }}
+                d="M72.8652 1C72.8652 1 -31.7309 89.3247 23.7013 124.692C79.7941 160.482 144.193 78.2363 220.935 91.7566C371.832 118.341 20.1657 295.907 158.468 342.435C295.034 388.379 457.5 178.122 457.5 178.122"
+                stroke="black"
+                strokeWidth={5}
+                strokeLinecap="round"
+              />
+            </g>
+            <defs>
+              <filter
+                id="filter0_d_2453_145"
+                x="0"
+                y="-2.5"
+                width="465"
+                height="363"
+                filterUnits="userSpaceOnUse"
+                colorInterpolationFilters="sRGB"
+              >
+                <feFlood floodOpacity="0" result="BackgroundImageFix" />
+                <feColorMatrix
+                  in="SourceAlpha"
+                  type="matrix"
+                  values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+                  result="hardAlpha"
+                />
+                <feOffset dy="4" />
+                <feGaussianBlur stdDeviation="2.5" />
+                <feComposite in2="hardAlpha" operator="out" />
+                <feColorMatrix
+                  type="matrix"
+                  values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"
+                />
+                <feBlend
+                  mode="normal"
+                  in2="BackgroundImageFix"
+                  result="effect1_dropShadow_2453_145"
+                />
+                <feBlend
+                  mode="normal"
+                  in="SourceGraphic"
+                  in2="effect1_dropShadow_2453_145"
                   result="shape"
                 />
               </filter>
