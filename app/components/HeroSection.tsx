8e5bbe6962ab0dbefd0fb2e35988eacc3de63f94
WAHYU AJI SETYO NUGROHO <wahyuajisn@gmail.com>
2026-06-20 11:47:27 +0700

    Update code

--- app/components/HeroSection.tsx
+++ app/components/HeroSection.tsx
@@ -35,6 +35,34 @@ export default function HeroSection() {
             isMounted ? "blur-0 scale-100" : "blur-lg scale-105"
           }`}
         />
+
+        <button
+          onClick={() => setOpenSidebar(!openSidebar)}
+          className="fixed left-6 top-6 z-30 flex h-10 w-10 items-center"
+        >
+          <span
+            className={`absolute h-0.5 bg-white transition-all duration-500 ease-in-out ${
+              openSidebar ? "w-7 rotate-45" : "w-7 -translate-y-2"
+            }`}
+          />
+          <span
+            className={`absolute h-0.5 bg-white transition-all duration-500 ease-in-out ${
+              openSidebar ? "opacity-0 scale-0" : "w-5 opacity-100"
+            }`}
+          />
+          <span
+            className={`absolute h-0.5 bg-white transition-all duration-500 ease-in-out ${
+              openSidebar ? "w-7 -rotate-45" : "w-7 translate-y-2"
+            }`}
+          />
+        </button>
+
+        <div
+          onClick={() => setOpenSidebar(false)}
+          className={`fixed z-20 inset-0 bg-black/40 backdrop-blur-sm transition-all duration-500 ${
+            openSidebar ? "opacity-100" : "pointer-events-none opacity-0"
+          }`}
+        />
       </div>
     </section>
   );
