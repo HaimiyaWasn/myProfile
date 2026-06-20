3184fabf156cde7f5601263f9f85b788bf8fc07b
WAHYU AJI SETYO NUGROHO <wahyuajisn@gmail.com>
2026-06-20 10:58:41 +0700

    Update code

--- app/components/HeroSection.tsx
+++ app/components/HeroSection.tsx
@@ -4,13 +4,15 @@ import Image from "next/image";
 import { useState, useEffect } from "react";
 
 import BackgroundDekstop from "@/public/img/background/1073991.jpg";
+import BackgroundMobile from "@/public/img/background/1192941.png";
 
 export default function HeroSection() {
   const [isMounted, setIsMounted] = useState(false);
+  const [openSidebar, setOpenSidebar] = useState(false);
 
   useEffect(() => {
     setIsMounted(true);
-  });
+  }, []);
 
   return (
     <section className="sticky top-0 h-screen overflow-hidden">
@@ -20,11 +22,19 @@ export default function HeroSection() {
           alt="Background Dekstop"
           fill
           priority
-          className={`object-cover transition-all duration-1500 ease-in-out ${
+          className={`hidden md:block object-cover transition-all duration-1500 ease-in-out ${
             isMounted ? "blur-0 scale-100" : "blur-lg scale-105"
           }`}
         />
-        <div className="absolute inset-0 bg-black/50" />
+        <Image
+          src={BackgroundMobile}
+          alt="Background Mobile"
+          fill
+          priority
+          className={`block md:hidden object-cover transition-all duration-1500 ease-in-out ${
+            isMounted ? "blur-0 scale-100" : "blur-lg scale-105"
+          }`}
+        />
       </div>
     </section>
   );
